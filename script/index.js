aa89f17cd6af2562e4a20f3e480a0bb8d6e322cb
Abu Bakor Siddik (Shuvo) <141806136+Abs-shuvo@users.noreply.github.com>
2026-03-29 19:13:26 +0600

    added full functionality of filter to hide and show each tab,added some condition,added a empty state

--- script/index.js
+++ script/index.js
@@ -5,8 +5,15 @@ let currentTab = 'all';
 const tabActive = ['bg-blue-500','border-blue-500','text-white'];
 const tabInactive = ['bg-transparent','text-slate-700','border-slate-200'];
 
+const allContainer = document.getElementById('all-container');
+const interviewContainer = document.getElementById('interview-container');
+const rejectedContainer = document.getElementById('rejected-container');
+
+console.log(allContainer,interviewContainer,rejectedContainer);
+
 function switchTab(tab){
     console.log(tab);
+
     const tabs = ['all','interview','rejected']
 
     for (const t of tabs) {
@@ -20,4 +27,20 @@ function switchTab(tab){
             tabName.classList.remove(...tabActive);            
         }
     }
+
+    const pages = [allContainer,interviewContainer,rejectedContainer];
+    for (const section of pages) {
+        section.classList.add('hidden');
+    }
+
+    if(tab === 'all'){
+        allContainer.classList.remove('hidden');
+    }
+    else if(tab === 'interview'){
+        interviewContainer.classList.remove('hidden');
+    }
+    else{
+        rejectedContainer.classList.remove('hidden');
+    }
 }
+switchTab(currentTab);
